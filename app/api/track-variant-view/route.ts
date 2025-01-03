1b58ab2a2262d4f7dca95471afeb0030dcbddaa0
Scott Weaver <scottmweaver@gmail.com>
2025-01-03 13:49:53 -0800

    (chore) Add console.error log to catch error

--- app/api/track-variant-view/route.ts
+++ app/api/track-variant-view/route.ts
@@ -1,21 +1,20 @@
-import { type NextRequest } from 'next/server';
+import { type NextRequest } from "next/server";
 
-import { increaseVariantViews } from '@/sanity/lib/helpers/increaseVariantViews';
+import { increaseVariantViews } from "@/sanity/lib/helpers/increaseVariantViews";
 
 export async function POST(request: NextRequest) {
-  const res = await request.json()
+  const res = await request.json();
   const { variantId } = res;
-  // const searchParams = request.nextUrl.searchParams
-  // const variantId = searchParams.get('variantId')
 
   if (!variantId) {
-    return new Response('Variant ID is required', {status: 400 });
+    return new Response("Variant ID is required", { status: 400 });
   }
 
   try {
-    await increaseVariantViews(variantId)
-  return new Response('Event recorded', {status: 200 });
+    await increaseVariantViews(variantId);
+    return new Response("Event recorded", { status: 200 });
   } catch (error) {
-    return new Response('Error recording event', {status: 500 });
+    console.error(error);
+    return new Response("Error recording event", { status: 500 });
   }
 }
